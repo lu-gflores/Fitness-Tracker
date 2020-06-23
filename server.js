e55ab0161d88ef5ef9b3ac92393af3f87f4a58c1
George Flores <georgeflores886@gmail.com>
2020-06-23 14:41:44 -0400

    modified index and server files

--- server.js
+++ server.js
@@ -5,15 +5,18 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const db = require("./models");
+
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true });
 
-
+// app.use(require("./routes/api-routes.js"));
+// app.use(require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
     console.log("App running on port " + PORT);
