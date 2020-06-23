b1cd034e13b413a42bfb58c7f02bb324f4c2a85f
George Flores <georgeflores886@gmail.com>
2020-06-23 15:43:57 -0400

    added routes to html pages

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -22,4 +23,15 @@ app.listen(PORT, () => {
     console.log("App running on port " + PORT);
 });
 
+//html routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'))
+});
 
+app.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/exercise.html'))
+});
+
+app.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/stats.html'))
+});
