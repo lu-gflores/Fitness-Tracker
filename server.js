863bdd6f13de086a782de4bc0966866445eff40a
George Flores <georgeflores886@gmail.com>
2020-06-23 14:28:53 -0400

    export workout model in index

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
+
+
 app.listen(PORT, () => {
     console.log("App running on port " + PORT);
 })
