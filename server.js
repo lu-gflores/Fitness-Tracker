345103ea6fbc688176b2b91aabbcc7346fe37806
George Flores <georgeflores886@gmail.com>
2020-06-22 20:19:42 -0400

    added mongoose package

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongojs = require("mongojs");
+const mongoose = require("mongoose");
+
 const app = express();
 const PORT = process.env.PORT || 8080;
+
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
 
 app.listen(PORT, () => {
     console.log("App running on port " + PORT);
